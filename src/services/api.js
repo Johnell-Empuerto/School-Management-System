044eb8ad6f0ab70adb53fdc8d660bdb5f56c249c
Johnell Empuerto <empuertojohnellchess@gmail.com>
2026-03-29 09:10:41 +0800

    added promotion

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3001/api",
+//   withCredentials: true,
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://school-management-system-backend-8tjk.onrender.com/api",
   withCredentials: true,
 });
 
